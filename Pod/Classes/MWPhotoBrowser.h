0c4e0951cd7619bc715b1c8d8d9ee0bc1ac37036
Timenote <charlescohen7@gmail.com>
2016-05-26 06:14:45 +0200

    Update MWPhotoBrowser.h

--- Pod/Classes/MWPhotoBrowser.h
+++ Pod/Classes/MWPhotoBrowser.h
@@ -24,6 +24,7 @@
 
 - (NSUInteger)numberOfPhotosInPhotoBrowser:(MWPhotoBrowser *)photoBrowser;
 - (id <MWPhoto>)photoBrowser:(MWPhotoBrowser *)photoBrowser photoAtIndex:(NSUInteger)index;
+- (void) deleteMyPhoto:(NSString *) photo_id index:(NSInteger) index;
 
 @optional
 
